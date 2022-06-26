c9ec7eec98cd20e38d7ef2ac07febb8806231acf
nazafiyah <sumaiahnaz2023@gmail.com>
2022-06-26 22:38:09 +0530

    addded env variable for port

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const morgan = require('morgan');
 const fs = require('fs');
 const path = require('path');
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 //registering routes.................
 const homeRouter = require('./routers/homeRouter');
